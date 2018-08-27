64d12088ff513c71e7587390bb1150ef60369844
wootiml33t <zachariah.s.leonard@gmail.com>
2018-08-27 16:36:15 -0700

    First commit

---
+++ RemotePhoneDialer/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace RemotePhoneDialer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_pound = new System.Windows.Forms.Button();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.button_star = new System.Windows.Forms.Button();
+            this.button_call = new System.Windows.Forms.Button();
+            this.button_end = new System.Windows.Forms.Button();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.button_back = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_5
+            // 
+            this.button_5.Location = new System.Drawing.Point(263, 261);
+            this.button_5.Name = "button_5";
+            this.button_5.Size = new System.Drawing.Size(118, 98);
+            this.button_5.TabIndex = 0;
+            this.button_5.Text = "5";
+            this.button_5.UseVisualStyleBackColor = true;
+            this.button_5.Click += new System.EventHandler(this.button_5_Click);
+            // 
+            // button_4
+            // 
+            this.button_4.Location = new System.Drawing.Point(118, 261);
+            this.button_4.Name = "button_4";
+            this.button_4.Size = new System.Drawing.Size(116, 98);
+            this.button_4.TabIndex = 1;
+            this.button_4.Text = "4";
+            this.button_4.UseVisualStyleBackColor = true;
+            this.button_4.Click += new System.EventHandler(this.button_4_Click);
+            // 
+            // button_9
+            // 
+            this.button_9.Location = new System.Drawing.Point(410, 363);
+            this.button_9.Name = "button_9";
+            this.button_9.Size = new System.Drawing.Size(118, 97);
+            this.button_9.TabIndex = 2;
+            this.button_9.Text = "9";
+            this.button_9.UseVisualStyleBackColor = true;
+            this.button_9.Click += new System.EventHandler(this.button_9_Click);
+            // 
+            // button_7
+            // 
+            this.button_7.Location = new System.Drawing.Point(118, 363);
+            this.button_7.Name = "button_7";
+            this.button_7.Size = new System.Drawing.Size(116, 97);
+            this.button_7.TabIndex = 3;
+            this.button_7.Text = "7";
+            this.button_7.UseVisualStyleBackColor = true;
+            this.button_7.Click += new System.EventHandler(this.button_7_Click);
+            // 
+            // button_8
+            // 
+            this.button_8.Location = new System.Drawing.Point(263, 363);
+            this.button_8.Name = "button_8";
+            this.button_8.Size = new System.Drawing.Size(118, 97);
+            this.button_8.TabIndex = 4;
+            this.button_8.Text = "8";
+            this.button_8.UseVisualStyleBackColor = true;
+            this.button_8.Click += new System.EventHandler(this.button_8_Click);
+            // 
+            // button_6
+            // 
+            this.button_6.Location = new System.Drawing.Point(410, 261);
+            this.button_6.Name = "button_6";
+            this.button_6.Size = new System.Drawing.Size(118, 96);
+            this.button_6.TabIndex = 5;
+            this.button_6.Text = "6";
+            this.button_6.UseVisualStyleBackColor = true;
+            this.button_6.Click += new System.EventHandler(this.button_6_Click);
+            // 
+            // button_3
+            // 
+            this.button_3.Location = new System.Drawing.Point(410, 153);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(118, 102);
+            this.button_3.TabIndex = 6;
+            this.button_3.Text = "3";
+            this.button_3.UseVisualStyleBackColor = true;
+            this.button_3.Click += new System.EventHandler(this.button_3_Click);
+            // 
+            // button_2
+            // 
+            this.button_2.Location = new System.Drawing.Point(263, 153);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(118, 102);
+            this.button_2.TabIndex = 7;
+            this.button_2.Text = "2";
+            this.button_2.UseVisualStyleBackColor = true;
+            this.button_2.Click += new System.EventHandler(this.button_2_Click);
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(118, 153);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(116, 102);
+            this.button_1.TabIndex = 8;
+            this.button_1.Text = " 1";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button_1_Click);
+            // 
+            // button_pound
+            // 
+            this.button_pound.Location = new System.Drawing.Point(118, 466);
+            this.button_pound.Name = "button_pound";
+            this.button_pound.Size = new System.Drawing.Size(116, 94);
+            this.button_pound.TabIndex = 9;
+            this.button_pound.Text = "#";
+            this.button_pound.UseVisualStyleBackColor = true;
+            this.button_pound.Click += new System.EventHandler(this.button_pound_Click);
+            // 
+            // button_0
+            // 
+            this.button_0.Location = new System.Drawing.Point(263, 466);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(118, 94);
+            this.button_0.TabIndex = 10;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = true;
+            this.button_0.Click += new System.EventHandler(this.button_0_Click);
+            // 
+            // button_star
+            // 
+            this.button_star.Location = new System.Drawing.Point(410, 466);
+            this.button_star.Name = "button_star";
+            this.button_star.Size = new System.Drawing.Size(118, 94);
+            this.button_star.TabIndex = 11;
+            this.button_star.Text = "*";
+            this.button_star.UseVisualStyleBackColor = true;
+            this.button_star.Click += new System.EventHandler(this.button_star_Click);
+            // 
+            // button_call
+            // 
+            this.button_call.Location = new System.Drawing.Point(118, 582);
+            this.button_call.Name = "button_call";
+            this.button_call.Size = new System.Drawing.Size(197, 122);
+            this.button_call.TabIndex = 12;
+            this.button_call.Text = "Call";
+            this.button_call.UseVisualStyleBackColor = true;
+            this.button_call.Click += new System.EventHandler(this.button_call_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(330, 582);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(198, 122);
+            this.button_end.TabIndex = 13;
+            this.button_end.Text = "End";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(118, 88);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(410, 38);
+            this.textBoxPhoneNumber.TabIndex = 14;
+            this.textBoxPhoneNumber.TextChanged += new System.EventHandler(this.textBoxPhoneNumber_TextChanged);
+            // 
+            // button_back
+            // 
+            this.button_back.Location = new System.Drawing.Point(543, 153);
+            this.button_back.Name = "button_back";
+            this.button_back.Size = new System.Drawing.Size(118, 102);
+            this.button_back.TabIndex = 15;
+            this.button_back.Text = "<";
+            this.button_back.UseVisualStyleBackColor = true;
+            this.button_back.Click += new System.EventHandler(this.button_back_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(543, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 102);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 716);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_back);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_call);
+            this.Controls.Add(this.button_star);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_pound);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_5);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_pound;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_star;
+        private System.Windows.Forms.Button button_call;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.Button button_back;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RemotePhoneDialer/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JustRemotePhone.RemotePhoneService;
+
+namespace RemotePhoneDialer
+{
+    public partial class Form1 : Form
+    {
+        JustRemotePhone.RemotePhoneService.Application app;
+        public Form1()
+        {
+
+            InitializeComponent();
+            app = new JustRemotePhone.RemotePhoneService.Application("Dialer-demo");
+            app.BeginConnect(true);
+            
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            inputValidator('1');
+        }
+
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            inputValidator('2');
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            inputValidator('3');
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            inputValidator('4');
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            inputValidator('5');
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            inputValidator('6');
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            inputValidator('7');
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            inputValidator('8');
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            inputValidator('9');
+        }
+
+        private void button_pound_Click(object sender, EventArgs e)
+        {
+            inputValidator('#');
+        }
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            inputValidator('0');
+        }
+
+        private void button_star_Click(object sender, EventArgs e)
+        {
+            inputValidator('*');
+        }
+
+        private void button_call_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputValidator(char keyIn)
+        {
+            if(/*not international &&*/ textBoxPhoneNumber.Text.Length <= 9)
+            {
+                textBoxPhoneNumber.Text += keyIn;
+            }
+            else if (false/*if in call*/)
+            {
+                textBoxPhoneNumber.Text += keyIn;
+                //dial keyIn
+            }
+        }
+
+        private void textBoxPhoneNumber_TextChanged(object sender, EventArgs e)
+        {
+            //if not international is checked then if text length is greater than 10 trim last digit
+            if(true/*international not checked*/) //this should be combined into the nested if conditions
+            {
+                if(textBoxPhoneNumber.Text.Length > 9)
+                {
+                    textBoxPhoneNumber.Text.TrimEnd();
+                }
+
+            }
+        }
+
+        private void button_back_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBoxPhoneNumber.Text))
+            {
+                textBoxPhoneNumber.Text.TrimEnd(textBoxPhoneNumber.Text[textBoxPhoneNumber.Text.Length - 1]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Guid smsId = new Guid();
+            string[] phoneNumbersToSendSMS = new string[1];
+            phoneNumbersToSendSMS[0] = "5555555555";
+            app.Phone.SendSMS(phoneNumbersToSendSMS, "test", out smsId);
+        }
+    }
+}
